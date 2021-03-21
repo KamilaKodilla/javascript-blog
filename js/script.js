f6b4345eb6817a57227745fd6a9a4a4d0d090bea
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-03-21 21:31:40 +0100

    Add functions calculate

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
     optArticleAuthorSelector = '.post-author',
-    optTagsListSelector = '.tags.list';
+    optCloudClassCount = 5,
+    optCloudClassPrefix = 'tag-size-';
 
   function generateTitleLinks(customSelector = ''){
 
@@ -87,9 +88,30 @@
 
   generateTitleLinks();
 
+  function calculateTagsParams(tags){
+    const params = {max: 0, min: 999999};
+    for(let tag in tags){
+      console.log(tag + ' is used ' + tags[tag] + ' times');
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    } else if(tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+  }
+    return params;
+  }
+
+  calculateTagsParams();
+
+  function calculateTagClass(count, params){
+  const classNumber = Math.floor( ( (count - params.min) / (params.max - params.min) ) * optCloudClassCount + 1 );
+
+  return optCloudClassPrefix + classNumber;
+  }
+
   function generateTags(){
-    /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
 
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
@@ -110,11 +132,13 @@
         /* add generated code to html variable */
         html = html + linkHTML;
 
-    /* [NEW] check if this link is NOT already in allTags */
-    if(allTags.indexOf(linkHTML) == -1){
-    /* [NEW] add generated code to allTags array */
-    allTags.push(linkHTML);
-    }
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags[tag]) {
+        /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
+          }
 
       /* END LOOP: for each tag */
       }
@@ -126,8 +150,24 @@
     /* [NEW] find list of tags in right column */
     const tagList = document.querySelector('.tags');
 
-    /* [NEW] add html from allTags to tagList */
-    tagList.innerHTML = allTags.join(' ');
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams)
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ') ' + '</a>';
+      const tagLinkHTML = '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">' + tag + '</a></li>' + ' ';
+      console.log('tagLinkHTML:', tagLinkHTML);
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+
 
   }
 
